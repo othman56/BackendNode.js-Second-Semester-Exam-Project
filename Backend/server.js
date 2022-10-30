be40db2634494fb012e750ea32f5e018452e0d4d
Usman Muhammad Hassan <hassanusman5667@gmail.com>
2022-10-30 22:45:29 +0100

    connected to my mongodb sucessfully

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,17 @@
 const express = require('express')
-const dotenv = require('dotenv').config
+const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+const Blogs = require('../Blogs')
+const routes = require('./routes/api/blogRoutes')
+const connectDb = require('./config/dbconfig')
+const colors = require('colors')
+connectDb()
 
 const app = express()
 
+app.use((express.json()))
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/blogs', require('./routes/api/blogRoutes'))
 
 app.listen(port, () => console.log(`server started on ${port}`))
---
+++ Blogs.js
@@ -0,0 +1,31 @@
+const Blogs = [
+    {
+        title: 'blog post 1',
+        description: 'tweets',
+        author: 'aji',
+        state: 'draft and published',
+        Read_count: 1,
+        Read_time: Date(),
+        Body:'body of blog post one'
+    },
+    {
+        title: 'blog post 2',
+        description: 'oil and gas',
+        author: 'musa',
+        state: 'draft and published',
+        Read_count: 20,
+        Read_time: Date(),
+        Body:'body of blog post two'
+    },
+    {
+        title: 'blog post 3',
+        description: 'technology',
+        author: 'abdul',
+        state: 'draft and published',
+        Read_count: 5,
+        Read_time: Date(),
+        Body:'body of blog post three'
+    }
+]
+
+module.exports = Blogs
